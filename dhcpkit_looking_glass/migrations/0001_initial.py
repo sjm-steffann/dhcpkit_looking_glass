28b465ffd5fbd5163e4eb06dfae4d8c41bafdc6d
Sander Steffann <sander@steffann.nl>
2015-11-09 22:42:33 +0100

    Reformat code

--- dhcpkit_looking_glass/migrations/0001_initial.py
+++ dhcpkit_looking_glass/migrations/0001_initial.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
     ]
 
@@ -18,10 +17,12 @@ class Migration(migrations.Migration):
                 ('interface_id', models.CharField(max_length=1024, blank=True)),
                 ('remote_id', models.CharField(max_length=1024, blank=True)),
                 ('last_request', models.TextField(null=True, blank=True)),
-                ('last_request_ll', models.GenericIPAddressField(null=True, protocol='ipv6', blank=True, verbose_name='Link-local address')),
+                ('last_request_ll', models.GenericIPAddressField(null=True, protocol='ipv6', blank=True,
+                                                                 verbose_name='Link-local address')),
                 ('last_request_ts', models.DateTimeField(null=True, blank=True, verbose_name='Last request timestamp')),
                 ('last_response', models.TextField(null=True, blank=True)),
-                ('last_response_ts', models.DateTimeField(null=True, blank=True, verbose_name='Last response timestamp')),
+                ('last_response_ts',
+                 models.DateTimeField(null=True, blank=True, verbose_name='Last response timestamp')),
             ],
             options={
                 'db_table': 'clients',
